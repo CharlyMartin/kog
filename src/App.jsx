22bb9dc46ce9eee6e346bc13f5069922dea5b009
Charly Martin <charly@hey.com>
2024-02-29 15:59:05 +0000

    Add tw-merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import React from "react";
 
+import { cn } from "../utils/cn";
+
 export default function App() {
   return (
     <main>
-      <h1 className="text-2xl text-stone-950">React + Tailwind + Vite</h1>
+      <h1 className={cn("text-2xl text-stone-950")}>React + Tailwind + Vite</h1>
     </main>
   );
 }
